21619a5d78879d7a7e4485211a7af06131aeb371
DengSir <tdaddon@163.com>
2018-10-23 15:11:56 +0800

    feat: add locale

---
+++ Localization/enUS.lua
@@ -0,0 +1,15 @@
+
+local L = LibStub('AceLocale-3.0'):NewLocale('tdBetterPremadeGroup', 'enUS', true)
+if not L then return end
+
+--@debug@
+--[[
+--@end-debug@
+@localization(locale=""enUS", format="lua_additive_table", table-name="L", same-key-is-true=true)@
+--@debug@
+--]]
+--@end-debug@
+
+--@debug@
+L['Members Count'] = true
+--@end-debug@
---
+++ Localization/zhCN.lua
@@ -0,0 +1,5 @@
+
+local L = LibStub('AceLocale-3.0'):NewLocale('tdBetterPremadeGroup', 'zhCN')
+if not L then return end
+
+L['Members Count'] = '队员数量'
